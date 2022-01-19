1e3ef4820e6175d5c612eb2313f9319917a56c4d
Yohann Mahrez <yohann.mahrez@gmail.com>
2022-01-19 19:41:50 +0100

    Création Page scandinave et ajout photosdans les ressources

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import PageStyle from './components/PageStyle';
+// import PageStyle from './components/PageStyle';
+import PageRoomScandinave from './components/PageRoomScandinave';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <PageStyle />
+      <PageRoomScandinave />
+      {/* <PageStyle /> */}
     </div>
   )
 }
